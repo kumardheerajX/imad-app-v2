8c7f913cdd46333bf30acbf0657add4d3e249513
kumardheerajX <kumardheerajX@gmail.com>
2017-02-21 14:45:54 +0000

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 
 // move the element
-var image=document.getElementById('madi');
+var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+1;
